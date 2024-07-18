8da8352738ee57d2e8f627c8b7f41a4be2c2c83c
suminhan123 <suminh324@gmail.com>
2024-07-18 13:11:01 +0900

    feat: 백준 뒤집기 문제 풀이

---
+++ baekjoon/1439_s5_뒤집기/뒤집기.js
@@ -0,0 +1,20 @@
+const s = require("fs").readFileSync("/dev/stdin").toString().trim();
+
+let result_zero = 0;
+let result_one = 0;
+
+const check_zero = s.split("1");
+
+check_zero.forEach((value) => {
+  if (value !== "") result_zero += 1;
+});
+const check_one = s.split("0");
+check_one.forEach((value) => {
+  if (value !== "") result_one += 1;
+});
+
+if (result_one < result_zero) {
+  console.log(result_one);
+} else {
+  console.log(result_zero);
+}
---
+++ baekjoon/1439_s5_뒤집기/뒤집기.py
@@ -0,0 +1,18 @@
+s = input()
+result_zero = 0
+result_one = 0
+check_zero = s.split("1")
+
+for char in check_zero:
+  if (char != ""):
+    result_zero += 1
+check_one = s.split("0")
+
+for char in check_one:
+  if (char != ""):
+    result_one += 1
+print(min(result_zero, result_one))
+
+# for i in range(len(s)-1):
+#   if s[i] != s[i+1]:
+#     cnt += 1
