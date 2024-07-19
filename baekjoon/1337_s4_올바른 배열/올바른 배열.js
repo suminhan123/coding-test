7717ccfb221cfb415d5a9da49bcc600147a250e2
suminhan123 <suminh324@gmail.com>
2024-07-19 12:09:08 +0900

    feat: 백준 올바른 배열 문제 풀이

---
+++ baekjoon/1337_s4_올바른 배열/올바른 배열.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const file = process.platform === "linux" ? "dev/stdin" : "./text.txt";
+const input = fs.readFileSync(file).toString().trim().split("\n");
+
+input.shift();
+
+const number = input.map(Number);
+
+number.sort(function (a, b) {
+  return a - b;
+});
+
+let minValue = 4;
+for (let i = 0; i < number.length - 1; i++) {
+  let temp = 0;
+  for (let j = number[i]; j < number[i] + 5; j++) {
+    if (!number.includes(j)) {
+      temp += 1;
+    }
+  }
+  if (temp < minValue) {
+    minValue = temp;
+  }
+}
+
+console.log(minValue);
---
+++ baekjoon/1337_s4_올바른 배열/올바른 배열.py
@@ -0,0 +1,19 @@
+import sys
+
+input = sys.stdin.readline
+
+t = int(input())
+number = [int(input()) for _ in range(t)]
+number.sort()
+
+minVal = 4
+
+for i in range(len(number) - 1):
+  temp = 0
+  for j in range(number[i], number[i] + 5):
+    if j not in number:
+      temp +=1
+  if temp < minVal:
+    minVal = temp
+
+print(minVal)
