67b21497ea8bbca9f29620cbb60b8ba21c3dbafe
suminhan123 <suminh324@gmail.com>
2024-08-19 20:59:02 +0900

    feat: 프로그래머스 순위 검색 문제 풀이(이진 탐색 => 효율성 테스트 통과O)

--- programmers/lv2/순위 검색/순위 검색.js
+++ programmers/lv2/순위 검색/순위 검색.js
@@ -1,44 +1,77 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/72412
-const solution = (info, query) => {
-  let result = [];
-  const is = info.map((value) => {
-    return value.split(" ");
-  });
-  const q = query.map((value) => {
-    return value
-      .split(" and ")
-      .map((values, index) => {
-        if (index === 3) {
-          return values.split(" ");
-        }
-        return values;
-      })
-      .flat();
-  });
 
-  for (let i = 0; i < q.length; i++) {
-    let count = 0;
-
-    for (let j = 0; j < is.length; j++) {
-      let isPass = true;
-
-      for (let t = 0; t < 4; t++) {
-        if (q[i][t] !== "-" && q[i][t] !== is[j][t]) {
-          isPass = false;
-          break;
-        }
-      }
-      if (parseInt(is[j][4]) < parseInt(q[i][4])) {
-        isPass = false;
-      }
-      if (isPass) {
-        count += 1;
-      }
+const getInfoObject = (info) => {
+  let object = {};
+  info.forEach((value) => {
+    const arr = value.split(" ");
+    const score = parseInt(arr.pop());
+    const key = arr.join("");
+    if (object[key]) {
+      object[key].push(score);
+    } else {
+      object[key] = [score];
     }
+  });
 
-    result.push(count);
+  for (const key in object) {
+    object[key].sort((a, b) => a - b);
   }
 
+  return object;
+};
+
+const getQueryArr = (query) => {
+  return query.map((value) => {
+    return value
+      .split(" and ")
+      .map((v, i) => {
+        if (i === 3) {
+          return v.split(" ");
+        }
+        return v;
+      })
+      .flat()
+      .filter((v) => v !== "-");
+  });
+};
+
+const binarySearch = (infoScoreArr, queryScore) => {
+  let left = 0;
+  let right = infoScoreArr.length;
+
+  while (left < right) {
+    const mid = Math.floor((left + right) / 2);
+
+    if (infoScoreArr[mid] < queryScore) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+
+  return left;
+};
+
+const solution = (info, query) => {
+  let result = [];
+  const infos = getInfoObject(info);
+  const infosKey = Object.keys(infos);
+  const queries = getQueryArr(query); // info에 있는 지 확인
+
+  queries.forEach((query) => {
+    const score = parseInt(query.pop());
+    const count = infosKey
+      .filter((infoKey) => query.every((q) => infoKey.includes(q)))
+
+      // infos[value]에 배열의 숫자가 score 숫자보다 작으면 빼야함
+      .reduce(
+        (acc, value) =>
+          acc + infos[value].length - binarySearch(infos[value], score),
+        0
+      );
+
+    result.push(count);
+  });
   return result;
 };
 const info = [
@@ -49,6 +82,7 @@ const info = [
   "java backend junior chicken 80",
   "python backend senior chicken 50",
 ];
+
 const query = [
   "java and backend and junior and pizza 100",
   "python and frontend and senior and chicken 200",
