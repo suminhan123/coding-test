47926a474d298163ee6fc441c0180c9088fe2f51
suminhan123 <suminh324@gmail.com>
2024-09-12 10:39:12 +0900

    feat: 프로그래머스 햄버거 만들기 문제 풀이(스택 => 시간 초과 해결)

--- programmers/lv1/햄버거 만들기/햄버거 만들기.js
+++ programmers/lv1/햄버거 만들기/햄버거 만들기.js
@@ -4,16 +4,28 @@
 // 1: 빵 2: 야채 3: 고기
 
 const solution = (ingredient = [2, 1, 1, 2, 3, 1, 2, 3, 1]) => {
-  let ingreStr = ingredient.join("");
+  let arr = [];
   let result = 0;
-  while (true) {
-    if (ingreStr.includes("1231")) {
-      ingreStr = ingreStr.replace("1231", "");
+
+  for (let i = 0; i < ingredient.length; i++) {
+    arr.push(ingredient[i]);
+    // arr.join("").includes("1231") => join 연산 후 탐색까지 수행하기에 시간 초과
+    if (arr.length >= 4 && arr.slice(-4).join("") === "1231") {
+      arr.splice(-4, 4);
       result += 1;
-    } else {
-      return result;
     }
   }
+  return result;
 };
 const ingredient = [2, 1, 1, 2, 3, 1, 2, 3, 1];
 console.log(solution(ingredient));
+
+// 좋은 풀이
+let count = 0;
+for (let i = 0; i < ingredient.length; i++) {
+  if (ingredient.slice(i, i + 4).join("") === "1231") {
+    count++;
+    ingredient.splice(i, 4);
+    i -= 3;
+  }
+}
