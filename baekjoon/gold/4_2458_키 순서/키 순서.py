3591005a4be7dc4fe89c74ad530e33beeac35ac2
suminhan123 <suminh324@gmail.com>
2024-11-08 22:01:51 +0900

    feat: 백준 키순서 문제 풀이(dfs => 시간 초과 해결)

--- baekjoon/gold/4_2458_키 순서/키 순서.py
+++ baekjoon/gold/4_2458_키 순서/키 순서.py
@@ -1,34 +1,27 @@
 # https://www.acmicpc.net/problem/2458
+import sys
+input = sys.stdin.readline
 
-n, m = map(int, input().split())
-INF = int(1e9)
-graph = [[INF] * (n+1) for _ in range(n+1)]
+n,m = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+# graph = [[], [3, 5], [], [4], [2, 6], [2, 4], []]
 
 for _ in range(m):
   a, b = map(int, input().split())
-  graph[a][b] = 1
+  graph[a].append(b)
 
-for a in range(1, n+1):
-  for b in range(1, n+1):
-    if a == b:
-      graph[a][b] = 0
+visited = [[0] * (n+1) for _ in range(n+1)]
 
-for k in range(1, n+1):
-  for i in range(1, n+1):
-    for j in range(1, n+1):
-      graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+def search(start, idx):
+  for i in graph[idx]:
+    if not visited[start][i]:
+      visited[start][i] = 1
+      visited[i][start] = 1
+      search(start, i)
 
-result = 0
-for node in range(1, n+1):
-  left = 0
-  right = 0
-  left = sum(1 for i in graph[node] if i != INF and i > 0)
+for i in range(1, n+1):
+  visited[i][i] = 1
+  search(i, i)
 
-  for i in range(1, n+1):
-    for j in range(1, n+1):
-      if j == node and graph[i][j] != INF and graph[i][j]> 0:
-        right += 1
-  if left + right == n-1:
-    result += 1
+result = sum([1 if sum(visited[i]) == n else 0 for i in range(1,n+1)])
 print(result)
-
