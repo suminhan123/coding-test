a95904f38844777d37dc43b419a4a1a30a57cf6e
suminhan123 <suminh324@gmail.com>
2024-11-13 17:09:05 +0900

    feat: 프로그래머스 점프와 순간 이동 문제 풀이(구현 => 효율성 테스트 통과O)

--- programmers/lv2/점프와 순간 이동/점프와 순간 이동.py
+++ programmers/lv2/점프와 순간 이동/점프와 순간 이동.py
@@ -1,11 +1,14 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/12980
-
 def solution(n):
-  INF = int(1e9)
-  dp = [INF] * (n+1)
-  dp[1] = 1
-  for i in range(2, n+1):
-    dp[i] = dp[i-1] + 1
-    if i % 2 == 0:
-      dp[i] = dp[i // 2]
-  return dp[n]
+  result = 0
+  if n % 2 != 0:
+    n -= 1
+    result += 1
+  while True:
+    n = n // 2
+    if n % 2 != 0:
+      n -= 1
+      result += 1
+    if n == 0:
+      break
+  return result
