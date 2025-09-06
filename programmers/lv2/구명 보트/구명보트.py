2d24ffcb2d21b347ce980a67b630a7d9747d3041
suminhan123 <suminh324@gmail.com>
2025-09-06 16:44:31 +0900

    feat: 프로그래머스 구명보트 문제 풀이(그리디 => 시간 초과 해결)

--- programmers/lv2/구명 보트/구명보트.py
+++ programmers/lv2/구명 보트/구명보트.py
@@ -1,7 +1,7 @@
 from itertools import combinations
 
 
-people = [70, 80, 50]
+people = [20,20,20,30,50,60]
 limit = 100
 
 def contain_arr (arr, target):
@@ -14,19 +14,18 @@ def contain_arr (arr, target):
   return True
 
 
-def solution (people = [70, 50, 80, 50], limit = 100):
+def solution (people, limit):
+  people = sorted(people)
+  start_idx = 0
+  end_idx = len(people) - 1
   answer = 0
-  people_cnt = len(people)
-  if sum(people) <= limit: return 1
 
-  for i in range(people_cnt - 1 , 0, -1):
-    combi_list =sorted([[sum(peo), [j for j in peo]]  for peo in list(combinations(people, i)) if sum(peo) <= limit], key=lambda x:x[0], reverse=True) 
-    for combi in combi_list:
-      total, people_list = combi
-      if contain_arr(people, people_list):
-        for p in people_list:
-          people.remove(p)
-        answer += 1
-  return answer
+  while start_idx < end_idx:
+    if people[start_idx] + people[end_idx] <= limit:
+      answer += 1
+      start_idx += 1
+    end_idx -= 1
+
+  return len(people) - answer
 
 print(solution(people, limit))
