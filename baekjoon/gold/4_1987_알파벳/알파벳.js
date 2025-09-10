91e857f285b98c3181883e604bcd44fab1386ca3
suminhan123 <suminh324@gmail.com>
2025-09-10 20:51:13 +0900

    feat: 백준 알파벳 문제 풀이(백트래킹 => 시간 초과 해결)

--- baekjoon/gold/4_1987_알파벳/알파벳.js
+++ baekjoon/gold/4_1987_알파벳/알파벳.js
@@ -17,25 +17,33 @@ const graph = [];
 for (let i = 0; i < r; i++) {
   graph.push(input[1 + i].trim().split(""));
 }
+function getASCIICode(str) {
+  return str.charCodeAt() - 65;
+}
 
-let q = [];
 const dx = [-1, 0, 1, 0];
 const dy = [0, 1, 0, -1];
-q.push([0, 0, [graph[0][0]]]);
+
+const visited = new Array(26).fill(false);
 
 let answer = -1;
-while (q.length) {
-  const [x, y, alpahSet] = q.shift();
+
+function search(x, y, depth) {
+  answer = Math.max(answer, depth);
   for (let i = 0; i < 4; i++) {
     let nx = x + dx[i];
     let ny = y + dy[i];
 
     if (nx >= 0 && nx < r && ny >= 0 && ny < c) {
-      if (!alpahSet.includes(graph[nx][ny])) {
-        q.push([nx, ny, alpahSet.concat([graph[nx][ny]])]);
-        answer = alpahSet.length + 1;
+      if (!visited[getASCIICode(graph[nx][ny])]) {
+        visited[getASCIICode(graph[nx][ny])] = true;
+        search(nx, ny, depth + 1);
+        visited[getASCIICode(graph[x][y])] = false;
       }
     }
   }
 }
+visited[getASCIICode(graph[0][0])] = true;
+search(0, 0, 1);
+
 console.log(answer);
